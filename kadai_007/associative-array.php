de0fa5f1605394f74243aaf91e5f8b66a6a8294e
Yokereba <kozottio1007@gmail.com>
2024-08-14 02:21:05 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vegi_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+        print_r($vegi_data);
+        ?>
+    </p>
+</body>
+
+</html>
